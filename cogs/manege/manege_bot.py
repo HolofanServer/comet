131149bf058f1337ad99c0447ee71fc691cfc783
FreeWiFi7749 <holofansaba@gmail.com>
2024-09-28 15:56:35 -0500

    add restart

--- cogs/manege/manege_bot.py
+++ cogs/manege/manege_bot.py
@@ -6,12 +6,29 @@ import platform
 import asyncio
 import time
 import os
+from dotenv import load_dotenv
 
 from utils import api
+
+load_dotenv()
+SERVICE_NAME = os.getenv("SERVICE_NAME")
 
 class ManagementBotCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    async def rstart_bot(self):
+        try:
+            if platform.system() == "Linux":
+                subprocess.Popen(["sudo", "systemctl", "restart", f"{SERVICE_NAME}.service"])
+            elif platform.system() == "Darwin":
+                subprocess.Popen(["/bin/sh", "-c", "sleep 1; exec python3 " + " ".join(sys.argv)])
+            else:
+                print("このOSはサポートされていません。")
+                return
+            await self.bot.close()
+        except Exception as e:
+            print(f"再起動中にエラーが発生しました: {e}")
 
     @commands.hybrid_command(name='ping', hidden=True)
     async def ping(self, ctx):
@@ -35,25 +52,11 @@ class ManagementBotCog(commands.Cog):
         e.add_field(name="Bot Ping", value=f"{bot_ping}ms", inline=True)
         await sent_message.edit(embed=e)
 
-    @commands.hybrid_command(name='embedtetst', hidden=True)
-    async def embedtest(self, ctx):
-        """Embedのテスト"""
-
-        e1 = discord.Embed(title="Botのバージョン", color=discord.Color.blue())
-        e1.add_field(name="Bot", value=f"{self.bot.user.name} {self.bot.user.id}")
-        e1.add_field(name="Python", value=f"{platform.python_version()}")
-        e1.add_field(name="discord.py", value=f"{discord.__version__}")
-
-        e2 = discord.Embed(title="システム情報", color=discord.Color.blue())
-        e2.add_field(name="OS", value=f"{platform.system()} {platform.release()}")
-        e2.add_field(name="CPU", value=f"{platform.processor()}")
-
-        e3 = discord.Embed(title="プロセス情報", color=discord.Color.blue())
-        e3.add_field(name="プロセスID", value=f"{os.getpid()}")
-
-        embed = [e1, e2, e3]
-
-        await ctx.send(embed=embed)
+    @commands.command(name='restart')
+    async def restart(self, ctx):
+        """Botを再起動します"""
+        await ctx.send("Botを再起動します...")
+        await self.bot.close()
 
 async def setup(bot):
     await bot.add_cog(ManagementBotCog(bot))
